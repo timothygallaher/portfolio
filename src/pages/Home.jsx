86711b77237f80c1e1e0daeeab819729e798791f
Timothy Gallaher <timothyjedidiah@gmail.com>
2025-05-02 00:48:00 -0700

    index.html update and Home page update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
       <div className="home">
         <img src="/profile.jpg" alt="Profile" />
-        <h2>Timothy Gallaher</h2>
+        <h2>Timothy Jedidiah Gallaher</h2>
         <p className="tagline">Front-end Developer and Pickleballer/Boulderhead</p>
       </div>
     );
